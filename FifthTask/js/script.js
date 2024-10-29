0b47ab96396c4c6af19b691ffe897377aee4a5d8
Egobam <95541782+Egobam@users.noreply.github.com>
2024-10-29 13:28:45 +0300

    Update dom

--- FifthTask/js/script.js
+++ FifthTask/js/script.js
@@ -1,18 +1,20 @@
-const calculateButton = document.getElementById('calculate');
-const quantityInput = document.getElementById('quantity');
-const productPriceInput = document.getElementById('product');
-const resultDisplay = document.getElementById('result');
-
-function calculateTotalCost() {
-    const quantity = parseFloat(quantityInput.value);
-    const productPrice = parseFloat(productPriceInput.value);
+document.addEventListener("DOMContentLoaded",() => {
+    const calculateButton = document.getElementById('calculate');
+    const quantityInput = document.getElementById('quantity');
+    const productPriceInput = document.getElementById('product');
+    const resultDisplay = document.getElementById('result');
     
-    if (!isNaN(quantity) && quantity >= 0 && !isNaN(productPrice) && productPrice > 0) {
-        const totalCost = quantity * productPrice;
-        resultDisplay.textContent = Стоимость заказа: ${totalCost};
-    } else {
-        resultDisplay.textContent = 'Пожалуйста, введите корректные данные';
+    function calculateTotalCost() {
+        const quantity = parseFloat(quantityInput.value);
+        const productPrice = parseFloat(productPriceInput.value);
+        
+        if (!isNaN(quantity) && quantity >= 0 && !isNaN(productPrice) && productPrice > 0) {
+            const totalCost = quantity * productPrice;
+            resultDisplay.textContent = Стоимость заказа: ${totalCost};
+        } else {
+            resultDisplay.textContent = 'Пожалуйста, введите корректные данные';
+        }
     }
-}
-
-calculateButton.addEventListener('click', calculateTotalCost);
+    
+    calculateButton.addEventListener('click', calculateTotalCost);
+})
